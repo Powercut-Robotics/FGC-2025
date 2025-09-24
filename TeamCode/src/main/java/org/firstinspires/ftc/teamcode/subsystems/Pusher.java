70562d4567d105a9d313c35ee86b692b4a56dd40
outsharded <92227828+outsharded@users.noreply.github.com>
2025-09-24 10:42:40 +0100

    Update Pusher.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pusher.java
@@ -10,20 +10,27 @@ public class Pusher implements Subsystem {
     public static final Pusher INSTANCE = new Pusher();
     private Pusher() { }
 
-    private ControlSystem pusherControlSystem = ControlSystem.builder()
-            .velPid(1)
-            .build();
+    private double power = 0;
+
+    //private ControlSystem pusherControlSystem = ControlSystem.builder()
+    //        .velPid(1)
+    //        .build();
 
     private MotorEx pusherMotor = new MotorEx("pusherMotor")
             .brakeMode();
 
-    public Command pushDownBalls = new RunToVelocity(pusherControlSystem, -100).requires(pusherMotor);
-    public Command holdBalls = new RunToVelocity(pusherControlSystem, 0).requires(pusherMotor);
-    public Command shootBalls = new RunToVelocity(pusherControlSystem, 100).requires(pusherMotor);
+    //public Command pushDownBalls = new RunToVelocity(pusherControlSystem, -100).requires(pusherMotor);
+    //public Command holdBalls = new RunToVelocity(pusherControlSystem, 0).requires(pusherMotor);
+    //public Command shootBalls = new RunToVelocity(pusherControlSystem, 100).requires(pusherMotor);
+
+    public Command pushDownBalls = new InstantCommand(() -> power = -1).requires(pusherMotor);
+    public Command holdBalls = new InstantCommand(() -> power = 0).requires(pusherMotor);
+    public Command pushUpBalls = new InstantCommand(() -> power = 1).requires(pusherMotor);
 
     @Override
     public void periodic() {
-        pusherMotor.setPower(pusherControlSystem.calculate(pusherMotor.getState()));
+        //pusherMotor.setPower(pusherControlSystem.calculate(pusherMotor.getState()));
+        pusherMotor.setPower(power);
     }
 
 }
